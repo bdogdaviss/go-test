ab6e60d006a1a53b712ba7248e06c1eed90c3ee7
bdogdaviss <baronmichaeldavis@gmail.com>
2026-05-02 05:39:22 -0500

    Add Discord color design system

---
+++ WakeupMobile/Core/DesignSystem/Color+Discord.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+
+extension Color {
+    static let discordBackground = Color(red: 0.09, green: 0.10, blue: 0.12)
+    static let discordSidebar = Color(red: 0.05, green: 0.06, blue: 0.08)
+    static let discordSurface = Color(red: 0.13, green: 0.14, blue: 0.17)
+    static let discordMessage = Color(red: 0.18, green: 0.19, blue: 0.23)
+    static let discordAccent = Color(red: 0.33, green: 0.54, blue: 0.98)
+}
