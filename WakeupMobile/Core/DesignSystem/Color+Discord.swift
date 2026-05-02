1bba73652347d1561a78a1b54bdd56d756319b26
bdogdaviss <baronmichaeldavis@gmail.com>
2026-05-02 05:49:40 -0500

    Refine Discord color palette

--- WakeupMobile/Core/DesignSystem/Color+Discord.swift
+++ WakeupMobile/Core/DesignSystem/Color+Discord.swift
@@ -1,9 +1,11 @@
 import SwiftUI
 
 extension Color {
-    static let discordBackground = Color(red: 0.09, green: 0.10, blue: 0.12)
+    static let discordBackground = Color(red: 0.07, green: 0.08, blue: 0.10)
     static let discordSidebar = Color(red: 0.05, green: 0.06, blue: 0.08)
-    static let discordSurface = Color(red: 0.13, green: 0.14, blue: 0.17)
-    static let discordMessage = Color(red: 0.18, green: 0.19, blue: 0.23)
-    static let discordAccent = Color(red: 0.33, green: 0.54, blue: 0.98)
+    static let discordSurface = Color(red: 0.11, green: 0.12, blue: 0.15)
+    static let discordMessage = Color(red: 0.16, green: 0.17, blue: 0.21)
+    static let discordAccent = Color(red: 0.35, green: 0.55, blue: 0.98)
+    static let discordTextPrimary = Color.white
+    static let discordTextSecondary = Color.white.opacity(0.65)
 }
