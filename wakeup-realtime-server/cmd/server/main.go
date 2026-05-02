ec875f0b5af3c746b910eb846459081427b6a72e
bdogdaviss <baronmichaeldavis@gmail.com>
2026-05-02 05:28:12 -0500

    Add Go server health endpoint

--- wakeup-realtime-server/cmd/server/main.go
+++ wakeup-realtime-server/cmd/server/main.go
@@ -3,19 +3,17 @@ package main
 import (
 	"log"
 	"net/http"
-
-	"github.com/bdogdaviss/go-test/wakeup-realtime-server/api"
-	"github.com/bdogdaviss/go-test/wakeup-realtime-server/internal/websocket"
 )
 
 func main() {
-	hub := websocket.NewHub()
-	go hub.Run()
+	mux := http.NewServeMux()
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("ok"))
+	})
 
-	router := api.NewRouter(hub)
-
-	log.Println("realtime server starting on :8080")
-	if err := http.ListenAndServe(":8080", router); err != nil {
+	log.Println("Wake Up realtime server starting on :8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
 		log.Fatal(err)
 	}
 }
